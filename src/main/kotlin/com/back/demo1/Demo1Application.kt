2af4f61972ed686bf2a0b7b035c16ff133242188
gioJioh <sjgio0515@naver.com>
2026-05-27 15:22:24 +0900

    init

---
+++ src/main/kotlin/com/back/demo1/Demo1Application.kt
@@ -0,0 +1,11 @@
+package com.back.demo1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo1Application
+
+fun main(args: Array<String>) {
+    runApplication<Demo1Application>(*args)
+}
